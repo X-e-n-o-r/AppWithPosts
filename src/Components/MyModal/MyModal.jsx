9bb7d4b704b1032b62cfb65c5b577608e1de8b66
Xenor <shokot_2005@mail.ru>
2023-04-04 22:22:57 +0300

    create post dissapears when you click on the dark field

--- src/Components/MyModal/MyModal.jsx
+++ src/Components/MyModal/MyModal.jsx
@@ -9,8 +9,8 @@ export default function MyModal({children, visible, setVisible}) {
     }
 
   return (
-    <div className={rootClasses.join(' ')}>
-      <div className={cl.myModalContent}>
+    <div className={rootClasses.join(' ')} onClick={() => {setVisible(false)}}>
+      <div className={cl.myModalContent} onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
