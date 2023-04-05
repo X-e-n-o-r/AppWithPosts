1e18a5a675c1a0f3af95d65233e6e719f5409bbd
Xenor <shokot_2005@mail.ru>
2023-04-05 21:49:51 +0300

    added animation and custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import PostItem from './Components/PostItem'
 import PostList from './Components/PostList'
 import PostFilter from './Components/PostFilter'
 import MyModal from './Components/MyModal/MyModal'
+import { usePosts } from './Components/Hooks/usePosts'
 
 function App() {
-  const [posts, setPosts] = useState([
-  ])
+  const [posts, setPosts] = useState([])
 
   const [filter, setFilter] = useState({
     sort: '', query: ''
@@ -17,17 +17,7 @@ function App() {
 
   const [modal, setModal] = useState(false)
 
-  const sortedPosts = useMemo(() => {
-    console.log('rerendered')
-    if (filter.sort) {
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts;
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
--- src/Components/PostList.jsx
+++ src/Components/PostList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PostItem from './PostItem'
-import { TransitionGroup } from 'react-transition-group'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 export default function PostList({posts, title, remove}) {
 
